ea1ad0c86d5f746c06157bf2b096de7e6f349fda
Fernando Escutia <fernando.escu9408@gmail.com>
2024-03-18 16:23:53 -0600

    REACT PJ

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./Styles/reset.css";
+import "./Styles/index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const books = [
+  {
+    tilte: "Fire & Blood: 300 Years Before A Game of Thrones",
+    author: "George R.R. Martin",
+    img: "https://m.media-amazon.com/images/I/91kXkSMY2OL._SL1500_.jpg",
+    id: 1,//?Este id es para que react no se confunda con los elementos
+  },
+  {
+    tilte:
+      "Game of Thrones: House of the Dragon: Inside the Creation of a Targaryen Dynastyt",
+    author: "George R.R. Martin",
+    img: "https://m.media-amazon.com/images/I/81WS74aySpL._SL1500_.jpg",
+    id: 2, //?Este id es para que react no se confunda con los elementos
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BoockList = () => {
+  //*Elementos que se van a colocar
+  return ( 
+    <section className="booklist">
+     {books.map((book)=>{
+      return(
+        <Book key={book.id} {...book} /> //?books.id es el id de cada libro, y el ...book es para pasarle todos los elementos del objeto book
+      )
+     })}
+    </section>
+  );
+};
+
+//*Que contienen los elementos que se van a poner y como estan acomodados
+const Book = ({ tilte, author, img}) => {  //?En estalinea estoy desestructurando los porps
+  return ( 
+    <article className="book">
+      <img src={img} alt={tilte} />
+      <h2>{tilte}</h2>
+      <h4>{author.toUpperCase()}</h4>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BoockList />); //?Aqui se esta renderizando el componente BoockList en el "root"
+ 
