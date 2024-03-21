37f537314f7239b5a6154878392c649e9037e736
Fernando Escutia <fernando.escu9408@gmail.com>
2024-03-21 09:44:05 -0600

    React Project

--- my-app/src/books.js
+++ my-app/src/books.js
@@ -1,16 +1,31 @@
+import imgb1 from "./Assets/Book-1.jpg";
+import imgb2 from "./Assets/Book-2.jpg";
+import imgb3 from "./Assets/Book-3.jpg";
+import imgb4 from "./Assets/Book-4.jpg";
 export const books = [
-    {
-      tilte: "Fire & Blood: 300 Years Before A Game of Thrones",
-      author: "George R.R. Martin",
-      img: "https://m.media-amazon.com/images/I/91kXkSMY2OL._SL1500_.jpg",
-      id: 1, //?Este id es para que react no se confunda con los elementos
-    },
-    {
-      tilte:
-        "Game of Thrones: House of the Dragon: Inside the Creation of a Targaryen Dynastyt",
-      author: "George R.R. Martin",
-      img: "https://m.media-amazon.com/images/I/81WS74aySpL._SL1500_.jpg",
-      id: 2, //?Este id es para que react no se confunda con los elementos
-    },
-  ];
-
+  {
+    title: "Fire & Blood: 300 Years Before A Game of Thrones",
+    author: "George R.R. Martin",
+    img: imgb1,
+    id: 1, //?Este id es para que react no se confunda con los elementos
+  },
+  {
+    title:
+      "Game of Thrones: House of the Dragon: Inside the Creation of a Targaryen Dynastyt",
+    author: "George R.R. Martin",
+    img: imgb2,
+    id: 2, //?Este id es para que react no se confunda con los elementos
+  },
+  {
+    title: "A Song of Ice and Fire: A Game of Thrones",
+    author: "George R.R. Martin",
+    img: imgb3,
+    id: 3,
+  },
+  {
+    title: "A Clash of Kings (A Song of Ice and Fire, Book 2)",
+    author: "George R.R. Martin",
+    img: imgb4,
+    id: 4,
+  },
+];
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,28 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./Styles/reset.css";
 import "./Styles/index.css";
-import {Book} from "./book";
-import {books} from "./books";
+import { Book } from "./book.js";
+import { books } from "./books.js";
 
 const BoockList = () => {
+  //?Este componente es el que se va a renderizar
   //*Elementos que se van a colocar
   const getBook = (id) => {
     const foundBook = books.find((book) => book.id === id);
     console.log(foundBook);
   };
   return (
-    <section className="booklist">
-      {books.map((book) => {
-        return (
-          <Book key={book.id} {...book} getBook={getBook} /> //?books.id es el id de cada libro, y el ...book es para pasarle todos los elementos del objeto book
-        );
-      })}
-    </section>
+    <>
+      <header>
+        <h1 className="Title">My favorite books</h1>
+      </header>
+
+      <section className="booklist">
+        {books.map((book, index) => {
+          return (
+            <Book
+              key={book.id}
+              {...book}
+              getBook={getBook}
+              number={index + 1}
+            /> //?books.id es el id de cada libro, y el ...book es para pasarle todos los elementos del objeto book
+          );
+        })}
+      </section>
+    </>
   );
 };
 
-
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(<BoockList />); //?Aqui se esta renderizando el componente BoockList en el "root"
